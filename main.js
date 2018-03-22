02f06c76621028c3f2c55f154de0dd2e1b69c0d4
Jasper Cussac <jasper.cussac@gmail.com>
2018-03-21 22:38:24 -0400

    Final Commit

--- main.js
+++ main.js
@@ -12,48 +12,92 @@ var cedarEl = document.getElementById('cedarButton');
 var baobabEl = document.getElementById('baobabButton');
 var resultsWrapperEl = document.getElementById('resultsWrapper');
 var resultsArray = resultsWrapperEl.children;
-var exitButton = document.getElementsByClassName("resultsCard-exit");
+var exitButton = document.getElementsByClassName('exitButton');
 
 console.log(resultsArray);
 
 spruceEl.addEventListener('click', function() {
-    displayResults('Spruce');
+  clearDisplay();
+  displayResults('Spruce');
+  window.scroll(0, 0); // For Chrome, Firefox, Opera
+  document.body.scrollTop = 0; // For Safari
 });
 pineEl.addEventListener('click', function() {
-    displayResults('Pine');
+  clearDisplay();
+  displayResults('Pine');
+  window.scroll(0, 0);
+  document.body.scrollTop = 0;
 });
 oakEl.addEventListener('click', function() {
-    displayResults('Oak');
+  clearDisplay();
+  displayResults('Oak');
+  window.scroll(0, 0);
+  document.body.scrollTop = 0;
 });
 redwoodEl.addEventListener('click', function() {
-    displayResults('Redwood');
+  clearDisplay();
+  displayResults('Redwood');
+  window.scroll(0, 0);
+  document.body.scrollTop = 0;
 });
 sequoiaEl.addEventListener('click', function() {
+  clearDisplay();
   displayResults('Sequoia');
+  window.scroll(0, 0);
+  document.body.scrollTop = 0;
 });
 palmEl.addEventListener('click', function() {
-    displayResults('Palm');
+  clearDisplay();
+  displayResults('Palm');
+  window.scroll(0, 0);
+  document.body.scrollTop = 0;
 });
 chestnutEl.addEventListener('click', function() {
-    displayResults('Chestnut');
+  clearDisplay();
+  displayResults('Chestnut');
+  window.scroll(0, 0);
+  document.body.scrollTop = 0;
 });
 weepingwillowEl.addEventListener('click', function() {
-    displayResults('Weeping Willow');
+  clearDisplay();
+  displayResults('Weeping Willow');
+  window.scroll(0, 0);
+  document.body.scrollTop = 0;
 });
 mapleEl.addEventListener('click', function() {
-    displayResults('Maple');
+  clearDisplay();
+  displayResults('Maple');
+  window.scroll(0, 0);
+  document.body.scrollTop = 0;
 });
 birchEl.addEventListener('click', function() {
-    displayResults('Birch');
+  clearDisplay();
+  displayResults('Birch');
+  window.scroll(0, 0);
+  document.body.scrollTop = 0;
 });
 cedarEl.addEventListener('click', function() {
-    displayResults('Cedar');
+  clearDisplay();
+  displayResults('Cedar');
+  window.scroll(0, 0);
+  document.body.scrollTop = 0;
 });
 baobabEl.addEventListener('click', function() {
-    displayResults('Baobab');
+  clearDisplay();
+  displayResults('Baobab');
+  window.scroll(0, 0);
+  document.body.scrollTop = 0;
 });
 
+for (var i = 0; i < exitButton.length; i++) {
+    exitButton[i].addEventListener('click', function() {
+        clearDisplay();
+        stopAllAudio();
+    });
+}
 
+
+// Zodiac algorythm starts here
 function calZodiac() {
 	birthdateObj = new Date(document.getElementById('birthdateEntry').value);
 
@@ -97,146 +141,159 @@ function calZodiac() {
     return false;
     console.log(birthdateObj);
 }
+// Zodiac algorythm ends here
+
+var activeTreeId = null;
 
 function timerHide(index) {
     setTimeout(function() {
         console.log("timerIndex",index);
-        if(index!==activeCharacterId){
+        if(index!==activeTreeId){
             resultsArray[index].classList.remove('display');
         }
     }, 1000);
 }
 
-
-var activeCharacterId = null;
-
-
+// Display results starts here
 function displayResults(treeType) {
+  stopAllAudio();
 
-    stopAllAudio();
+  console.log('displayResults()');
+  var i = 0;
 
-    console.log('displayResults()');
-    var i = 0;
+  for (var i = 0; i < resultsArray.length; i++) {
+      timerHide(i);
+  }
+
+  if (treeType === 'Spruce') {
+      resultsArray[1].classList.add('resultsOpacityDisplay');
+      resultsArray[1].classList.add('stackOrder');
+      setTimeout(function() {
+          resultsArray[1].classList.remove('stackOrder');
+          console.log('remove stackOrder class');
+      }, 1000);
+      activeTreeId = 1;
+      document.getElementById('spruceAudio').play();
+  } else if (treeType === 'Pine') {
+      resultsArray[2].classList.add('resultsOpacityDisplay');
+      resultsArray[2].classList.add('stackOrder');
+      setTimeout(function() {
+          resultsArray[2].classList.remove('stackOrder');
+      }, 1000);
+      activeTreeId= 2;
+      document.getElementById('pineAudio').play();
+  } else if (treeType === 'Oak') {
+      resultsArray[3].classList.add('resultsOpacityDisplay');
+      resultsArray[3].classList.add('stackOrder');
+      setTimeout(function() {
+          resultsArray[3].classList.remove('stackOrder');
+      }, 1000);
+      activeTreeId = 3;
+      document.getElementById('oakAudio').play();
+  } else if (treeType === 'Redwood') {
+      resultsArray[4].classList.add('resultsOpacityDisplay');
+      resultsArray[4].classList.add('stackOrder');
+      setTimeout(function() {
+          resultsArray[4].classList.remove('stackOrder');
+          console.log('remove stackOrder class');
+      }, 1000);
+      activeTreeId = 4;
+      document.getElementById('andy-audio').play();
+  } else if (treeType === 'Sequoia') {
+      resultsArray[5].classList.add('resultsOpacityDisplay');
+      resultsArray[5].classList.add('stackOrder');
+      setTimeout(function() {
+          resultsArray[5].classList.remove('stackOrder');
+          console.log('remove stackOrder class');
+      }, 1000);
+      activeTreeId = 5;
+      document.getElementById('andy-audio').play();
+  } else if (treeType === 'Palm') {
+      resultsArray[6].classList.add('resultsOpacityDisplay');
+      resultsArray[6].classList.add('stackOrder');
+      setTimeout(function() {
+          resultsArray[6].classList.remove('stackOrder');
+          console.log('remove stackOrder class');
+      }, 1000);
+      activeTreeId = 6;
+      document.getElementById('andy-audio').play();
+  } else if (treeType === 'Chestnut') {
+      resultsArray[7].classList.add('resultsOpacityDisplay');
+      resultsArray[7].classList.add('stackOrder');
+      setTimeout(function() {
+          resultsArray[7].classList.remove('stackOrder');
+          console.log('remove stackOrder class');
+      }, 1000);
+      activeTreeId = 7;
+      document.getElementById('chestnutAudio').play();
+  } else if (treeType === 'Weeping Willow') {
+      resultsArray[8].classList.add('resultsOpacityDisplay');
+      resultsArray[8].classList.add('stackOrder');
+      setTimeout(function() {
+          resultsArray[8].classList.remove('stackOrder');
+          console.log('remove stackOrder class');
+      }, 1000);
+      activeTreeId = 8;
+      document.getElementById('weepingwillowAudio').play();
+  } else if (treeType === 'Maple') {
+      resultsArray[9].classList.add('resultsOpacityDisplay');
+      resultsArray[9].classList.add('stackOrder');
+      setTimeout(function() {
+          resultsArray[9].classList.remove('stackOrder');
+          console.log('remove stackOrder class');
+      }, 1000);
+      activeTreeId = 9;
+      document.getElementById('').play();
+  } else if (treeType === 'Birch') {
+      resultsArray[10].classList.add('resultsOpacityDisplay');
+      resultsArray[10].classList.add('stackOrder');
+      setTimeout(function() {
+          resultsArray[10].classList.remove('stackOrder');
+          console.log('remove stackOrder class');
+      }, 1000);
+      activeTreeId = 10;
+      document.getElementById('birchAudio').play();
+  } else if (treeType === 'Cedar') {
+      resultsArray[11].classList.add('resultsOpacityDisplay');
+      resultsArray[11].classList.add('stackOrder');
+      setTimeout(function() {
+          resultsArray[11].classList.remove('stackOrder');
+          console.log('remove stackOrder class');
+      }, 1000);
+      activeTreeId = 11;
+      document.getElementById('cedarAudio').play();
+  } else if (treeType === 'Baobab') {
+      resultsArray[12].classList.add('resultsOpacityDisplay');
+      resultsArray[12].classList.add('stackOrder');
+      setTimeout(function() {
+          resultsArray[12].classList.remove('stackOrder');
+          console.log('remove stackOrder class');
+      }, 1000);
+      activeTreeId = 12;
+      document.getElementById('baobabAudio').play();
+  } else {
+      resultsArray[0].classList.add('resultsOpacityDisplay');
+      resultsArray[0].classList.add('stackOrder');
+      setTimeout(function() {
+          resultsArray[0].classList.remove('stackOrder');
+          console.log('remove stackOrder class');
+      }, 1000);
+      activeTreeId = 0;
+  }
+}
+// Display results ends here
+
+// Clear results starts here
+function clearDisplay() {
     for (var i = 0; i < resultsArray.length; i++) {
-        // resultsArray[i].classList.remove('display');
-        timerHide(i);
-    }
-
-    if (treeType === 'Spruce') {
-        resultsArray[1].classList.add('display');
-        resultsArray[1].classList.add('stackOrder');
-        setTimeout(function() {
-            resultsArray[1].classList.remove('stackOrder');
-            console.log('remove stackOrder class');
-        }, 1000);
-        activeCharacterId = 1;
-    } else if (treeType === 'Pine') {
-        resultsArray[2].classList.add('display');
-        resultsArray[2].classList.add('stackOrder');
-        setTimeout(function() {
-            resultsArray[2].classList.remove('stackOrder');
-        }, 1000);
-        activeCharacterId= 2;
-    } else if (treeType === 'Oak') {
-        resultsArray[3].classList.add('display');
-        resultsArray[3].classList.add('stackOrder');
-        setTimeout(function() {
-            resultsArray[3].classList.remove('stackOrder');
-        }, 1000);
-        activeCharacterId = 3;
-    } else if (treeType === 'Redwood') {
-        resultsArray[4].classList.add('display');
-        resultsArray[4].classList.add('stackOrder');
-        setTimeout(function() {
-            resultsArray[4].classList.remove('stackOrder');
-            console.log('remove stackOrder class');
-        }, 1000);
-        activeCharacterId = 4;
-    } else if (treeType === 'Sequoia') {
-        resultsArray[5].classList.add('display');
-        resultsArray[5].classList.add('stackOrder');
-        setTimeout(function() {
-            resultsArray[5].classList.remove('stackOrder');
-            console.log('remove stackOrder class');
-        }, 1000);
-        activeCharacterId = 5;
-    } else if (treeType === 'Palm') {
-        resultsArray[6].classList.add('display');
-        resultsArray[6].classList.add('stackOrder');
-        setTimeout(function() {
-            resultsArray[6].classList.remove('stackOrder');
-            console.log('remove stackOrder class');
-        }, 1000);
-        activeCharacterId = 6;
-    } else if (treeType === 'Chestnut') {
-        resultsArray[7].classList.add('display');
-        resultsArray[7].classList.add('stackOrder');
-        setTimeout(function() {
-            resultsArray[7].classList.remove('stackOrder');
-            console.log('remove stackOrder class');
-        }, 1000);
-        activeCharacterId = 7;
-    } else if (treeType === 'Weeping Willow') {
-        resultsArray[8].classList.add('display');
-        resultsArray[8].classList.add('stackOrder');
-        setTimeout(function() {
-            resultsArray[8].classList.remove('stackOrder');
-            console.log('remove stackOrder class');
-        }, 1000);
-        activeCharacterId = 8;
-    } else if (treeType === 'Maple') {
-        resultsArray[9].classList.add('display');
-        resultsArray[9].classList.add('stackOrder');
-        setTimeout(function() {
-            resultsArray[9].classList.remove('stackOrder');
-            console.log('remove stackOrder class');
-        }, 1000);
-        activeCharacterId = 9;
-    } else if (treeType === 'Birch') {
-        resultsArray[10].classList.add('display');
-        resultsArray[10].classList.add('stackOrder');
-        setTimeout(function() {
-            resultsArray[10].classList.remove('stackOrder');
-            console.log('remove stackOrder class');
-        }, 1000);
-        activeCharacterId = 10;
-    } else if (treeType === 'Cedar') {
-        resultsArray[11].classList.add('display');
-        resultsArray[11].classList.add('stackOrder');
-        setTimeout(function() {
-            resultsArray[11].classList.remove('stackOrder');
-            console.log('remove stackOrder class');
-        }, 1000);
-        activeCharacterId = 11;
-    } else if (treeType === 'Baobab') {
-        resultsArray[12].classList.add('display');
-        resultsArray[12].classList.add('stackOrder');
-        setTimeout(function() {
-            resultsArray[12].classList.remove('stackOrder');
-            console.log('remove stackOrder class');
-        }, 1000);
-        activeCharacterId = 12;
-    } else {
-        resultsArray[0].classList.add('display');
-        resultsArray[0].classList.add('stackOrder');
-        setTimeout(function() {
-            resultsArray[0].classList.remove('stackOrder');
-            console.log('remove stackOrder class');
-        }, 1000);
-        activeCharacterId = 0;
+        resultsArray[i].classList.remove('resultsOpacityDisplay');
+        resultsArray[i].classList.remove('stackOrder')
     }
 }
-
-
 function stopAllAudio() {
     var audio = document.getElementsByTagName('audio');
     for (i = 0; i < audio.length; i++) {
         audio[i].pause();
     }
 }
-
-function clearDisplay() {
-    for (var i = 0; i < resultsArray.length; i++) {
-        resultsArray[i].classList.remove('display');
-    }
-}
+// Clear results ends here
